b061ce93c7b58d11d4f8e41d60d2949f2bce352a
eplay1602-afk <eplay1602@gmail.com>
2026-06-11 20:12:14 +0500

    Initialize Discord bot with message handling

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Бот запущен как ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('🏓 Pong!');
+  }
+
+  if (message.content === '!arena') {
+    message.reply('🔥 ARENA FACEIT работает!');
+  }
+});
+
+client.login(process.env.TOKEN);
